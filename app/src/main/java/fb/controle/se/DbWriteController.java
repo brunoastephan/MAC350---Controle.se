0a9663b3b27e9d4bfe15e76b3cef875b4eea8e1d
Fernando-crz <fernando.cruz.ctt@gmail.com>
2024-05-14 23:16:18 -0300

    adiciona funções básicas no writer

--- app/src/main/java/fb/controle/se/DbWriteController.java
+++ app/src/main/java/fb/controle/se/DbWriteController.java
@@ -17,13 +17,34 @@ public class DbWriteController{
 
     public void close() {this.database.close();}
 
-    public void addElement(String mail, String phone) {
+    public void addTransaction(String date, float value, int category_id) {
         this.open();
         ContentValues values = new ContentValues();
-        values.put("mail", mail);
-        values.put("phone", phone);
+        values.put("date", date);
+        values.put("value", value);
+        values.put("category_id", category_id);
 
-        this.database.insert(dbHelper.getTableName(), null, values);
+        this.database.insert(dbHelper.getTransactionsTableName(), null, values);
+    }
+
+    public void addCategory(String name, String icon) {
+        this.open();
+        ContentValues values = new ContentValues();
+        values.put("name", name);
+        values.put("icon", icon);
+
+        this.database.insert(dbHelper.getCategoriesTableName(), null, values);
+    }
+
+    public void addGoal(String due_date, float expense_limit, int category_id) {
+        this.open();
+        ContentValues values = new ContentValues();
+        values.put("due_date", due_date);
+        values.put("expense_limit", expense_limit);
+        values.put("category_id", category_id);
+
+        this.database.insert(dbHelper.getTransactionsTableName(), null, values);
+
     }
 
 }
