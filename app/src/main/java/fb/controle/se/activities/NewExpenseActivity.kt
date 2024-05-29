7fef7ffed3e3eaa802524c77485fbc67d9b9fb91
Fernando-crz <fernando.cruz.ctt@gmail.com>
2024-05-29 16:47:29 -0300

    Retira barra com nome do aplicativo em tela de transaction

--- app/src/main/java/fb/controle/se/activities/NewExpenseActivity.kt
+++ app/src/main/java/fb/controle/se/activities/NewExpenseActivity.kt
@@ -8,5 +8,7 @@ class NewExpenseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_expense)
+
+        supportActionBar?.hide()
     }
 }
