af72b4e33462cd42ca8780f1d4908a93f7077781
Fernando-crz <fernando.cruz.ctt@gmail.com>
2024-06-12 17:27:31 -0300

    refatora MainActivity

--- app/src/main/java/fb/controle/se/activities/MainActivity.kt
+++ app/src/main/java/fb/controle/se/activities/MainActivity.kt
@@ -3,7 +3,6 @@ package fb.controle.se.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -11,11 +10,8 @@ import fb.controle.se.R
 import fb.controle.se.database.DbHelper
 import fb.controle.se.database.DbTransactionReader
 import fb.controle.se.database.DbWriteController
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.Month
 
-enum class TrasactionViewState {
+enum class TransactionViewState {
     DAY, MONTH, YEAR
 }
 
@@ -26,60 +22,55 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnYear : Button
     private lateinit var transactionTotalView : TextView
 
-    private var TRANSACTION_VIEW_STATE = TrasactionViewState.YEAR
+    private var transactionViewState = TransactionViewState.YEAR
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    private fun setupTransactionTotalView() {
+        transactionTotalView = findViewById(R.id.TransactionTotalView)
 
+        btnDay = findViewById(R.id.visualizeDayButton)
+        btnMonth = findViewById(R.id.visualizeMonthButton)
+        btnYear = findViewById(R.id.visualizeYearButton)
+
+        // no need to keep updating the transaction totals, since adding/removing transactions is in another activity
+        val readController = DbTransactionReader(this)
+        val transactionsTotalDay : Float = readController.readTransactionsTotalFromIds(readController.readTransactionsInDay())
+        val transactionsTotalMonth : Float = readController.readTransactionsTotalFromIds(readController.readTransactionsInMonth())
+        val transactionsTotalYear : Float = readController.readTransactionsTotalFromIds(readController.readTransactionsInYear())
+
+        val transactionsTotalDayFormatted : String = getString(R.string.transaction_total).format(transactionsTotalDay)
+        val transactionsTotalMonthFormatted : String = getString(R.string.transaction_total).format(transactionsTotalMonth)
+        val transactionsTotalYearFormatted : String = getString(R.string.transaction_total).format(transactionsTotalYear)
+
+        btnDay.setOnClickListener {
+            transactionViewState = TransactionViewState.DAY
+            transactionTotalView.text = transactionsTotalDayFormatted
+        }
+        btnMonth.setOnClickListener {
+            transactionViewState = TransactionViewState.MONTH
+            transactionTotalView.text = transactionsTotalMonthFormatted
+        }
+        btnYear.setOnClickListener {
+            transactionViewState = TransactionViewState.YEAR
+            transactionTotalView.text = transactionsTotalYearFormatted
+        }
+    }
+
+    private fun setupTransactionFloatingButton() {
         btnAddTrans = findViewById(R.id.addTransactionFloatingButton)
         btnAddTrans.setOnClickListener {
             val intent = Intent(this, NewExpenseActivity::class.java)
             startActivity(intent)
             finish()
         }
+    }
 
-//        DbHelper(this).deleteDatabase()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        val writer = DbWriteController(this)
-        writer.addCategory("DummyCat", "12")
-
-        val readController = DbTransactionReader(this)
-
-        val transactionsTotalDay : Float = readController.readTransactionsTotalFromIds(readController.readTransactionsInDay())
-        val transactionsTotalMonth : Float = readController.readTransactionsTotalFromIds(readController.readTransactionsInMonth())
-        val transactionsTotalYear : Float = readController.readTransactionsTotalFromIds(readController.readTransactionsInYear())
-
-        btnDay = findViewById(R.id.visualizeDayButton)
-        btnMonth = findViewById(R.id.visualizeMonthButton)
-        btnYear = findViewById(R.id.visualizeYearButton)
-
-        transactionTotalView = findViewById(R.id.TransactionTotalView)
-
-        btnDay.setOnClickListener {
-            TRANSACTION_VIEW_STATE = TrasactionViewState.DAY
-            transactionTotalView.text = getString(R.string.transaction_total).format(transactionsTotalDay)
-        }
-        btnMonth.setOnClickListener {
-            TRANSACTION_VIEW_STATE = TrasactionViewState.MONTH
-            transactionTotalView.text = getString(R.string.transaction_total).format(transactionsTotalMonth)
-        }
-        btnYear.setOnClickListener {
-            TRANSACTION_VIEW_STATE = TrasactionViewState.YEAR
-            transactionTotalView.text = getString(R.string.transaction_total).format(transactionsTotalYear)
-        }
-
-
-//        val transactionsInRange = readController.readTransactionsInTimeInterval(LocalDateTime.of(2023, 12, 25, 6, 23), LocalDateTime.now())
-//        Log.i("SQL", "transactions in datetime range: $transactionsInRange")
-//        Log.i("SQL", "transaction total: ${readController.readTransactionsTotal()}")
-//        Log.i("SQL", "transactions in day: ${readController.readTransactionsInDay()}")
-//        Log.i("SQL", "transaction in month: ${readController.readTransactionsInMonth()}")
-//        Log.i("SQL", "transaction in year: ${readController.readTransactionsInYear()}")
-//
+        setupTransactionTotalView()
+        setupTransactionFloatingButton()
 
         supportActionBar?.hide()
-
-
     }
 }
