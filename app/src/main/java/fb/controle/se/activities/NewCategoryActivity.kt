1d32201fbe527d1450bee7ad189d3534ba1178df
brunoastephan <brunoastephan@gmail.com>
2024-06-16 22:13:21 -0300

    criando funcionalidades da atividade de nova categoria

--- app/src/main/java/fb/controle/se/activities/NewCategoryActivity.kt
+++ app/src/main/java/fb/controle/se/activities/NewCategoryActivity.kt
@@ -7,22 +7,44 @@ import android.util.Log
 import android.view.Display
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import fb.controle.se.R
 import fb.controle.se.database.DbWriteController
 import java.time.LocalDateTime
 
 class NewCategoryActivity : AppCompatActivity() {
+
+    private lateinit var newCatTextInput: EditText
+    private lateinit var newIconTextInput: EditText
+
+    private lateinit var dbWriteController: DbWriteController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_category)
+        dbWriteController = DbWriteController(this)
 
+        initializeButtons()
+
+        newCatTextInput = findViewById(R.id.NewCatText)
+        newIconTextInput = findViewById(R.id.NewIconText)
+
+        supportActionBar?.hide()
+    }
+
+    private fun initializeButtons() {
         val btnBack = findViewById<Button>(R.id.btn_back)
         btnBack.setOnClickListener {
             backToMainActivity()
         }
 
-        supportActionBar?.hide()
+        val btnSubmit = findViewById<Button>(R.id.btn_submit)
+        btnSubmit.setOnClickListener {
+            if (newCatTextInput.text.isNotEmpty() && newIconTextInput.text.isNotEmpty()) {
+                dbWriteController.addCategory(newCatTextInput.text.toString(), newIconTextInput.text.toString())
+                backToMainActivity()
+            }
+        }
     }
 
     private fun backToMainActivity() {
